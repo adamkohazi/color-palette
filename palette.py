66248c9b0c7169202a801570de17935d66848f0a
adamkohazi <109677549+adamkohazi@users.noreply.github.com>
2025-05-27 16:41:44 +0200

    Small progress on generation methods. (not working)

--- palette.py
+++ palette.py
@@ -1,6 +1,7 @@
 #palette.py
 import color
 import random
+import math
 
 class Palette(object):
     def __init__(self):
@@ -32,3 +33,12 @@ class Palette(object):
     
     def generate_RGB_random(self):
         self._colors = [color.Color.from_RGB((random.random(), random.random(), random.random())) for _ in self._colors]
+    
+    def generate_RGB_cosine(self, a, b, c, d):
+        dp = math.pi * 2.0 / len(self._colors)
+        for index in range(len(self._colors)):
+            phase = index * dp
+            red = a[0] + b[0] * math.cos(c[0] * phase + d[0])
+            green = a[1] + b[1] * math.cos(c[1] * phase + d[1])
+            blue = a[2] + b[2] * math.cos(c[2] * phase + d[2])
+            self._colors[index] = color.Color.from_RGB((red, green, blue))
