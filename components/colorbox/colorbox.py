fb81dff138905b47a794dd01b70c1390f20b0dce
adamkohazi <109677549+adamkohazi@users.noreply.github.com>
2025-06-04 15:20:12 +0200

    Color mode selection for table, more color models

--- components/colorbox/colorbox.py
+++ components/colorbox/colorbox.py
@@ -8,8 +8,8 @@ from kivy.properties import ObjectProperty, OptionProperty, StringProperty, Colo
 class ColorBox(BoxLayout):
     # These need to be set from outside
     color = ObjectProperty(None)
-    color_model_1 = OptionProperty('RGB', options=['XYZ', 'RGB', 'HSB', 'CIELAB', 'OKLAB'])
-    color_model_2 = OptionProperty('XYZ', options=['XYZ', 'RGB', 'HSB', 'CIELAB', 'OKLAB'])
+    color_model_1 = OptionProperty('sRGB', options=['RGB', 'sRGB', '24-bit sRGB', 'HSB', 'XYZ', 'CIELAB'])
+    color_model_2 = OptionProperty('XYZ', options=['RGB', 'sRGB', '24-bit sRGB', 'HSB', 'XYZ', 'CIELAB'])
 
     # Background and hex code
     background_color = ColorProperty([1, 1, 1, 1]) # Background color
@@ -32,18 +32,22 @@ class ColorBox(BoxLayout):
             pass
     
     def update(self):
-        rgba = self.color.to_RGBA()
+        rgba = self.color.to_sRGBA()
         self.background_color = rgba
 
-        rgb = self.color.to_RGB255()
+        rgb = self.color.to_sRGB255()
         self.rgb_hex = '#{:02X}{:02X}{:02X}'.format(*rgb)
 
-        if self.color_model_1 == 'XYZ': 
-            color_components = self.color.to_XYZ()
-        elif self.color_model_1 == 'RGB': 
+        if self.color_model_1 == 'RGB': 
             color_components = self.color.to_RGB()
+        elif self.color_model_1 == 'sRGB': 
+            color_components = self.color.to_sRGB()
+        elif self.color_model_1 == '24-bit sRGB': 
+            color_components = self.color.to_sRGB255()
         elif self.color_model_1 == 'HSB': 
             color_components = self.color.to_HSB()
+        elif self.color_model_1 == 'XYZ': 
+            color_components = self.color.to_XYZ()
         elif self.color_model_1 == 'CIELAB': 
             color_components = self.color.to_CIELAB()
         #elif self.color_model_1 == 'OKLAB': 
@@ -53,15 +57,19 @@ class ColorBox(BoxLayout):
         self.color_model_1_component_2 = '{:.2f}'.format(color_components[1])
         self.color_model_1_component_3 = '{:.2f}'.format(color_components[2])
 
-        if self.color_model_2 == 'XYZ': 
-            color_components = self.color.to_XYZ()
-        elif self.color_model_2 == 'RGB': 
+        if self.color_model_2 == 'RGB': 
             color_components = self.color.to_RGB()
+        elif self.color_model_2 == 'sRGB': 
+            color_components = self.color.to_sRGB()
+        elif self.color_model_2 == '24-bit sRGB': 
+            color_components = self.color.to_sRGB255()
         elif self.color_model_2 == 'HSB': 
             color_components = self.color.to_HSB()
+        elif self.color_model_2 == 'XYZ': 
+            color_components = self.color.to_XYZ()
         elif self.color_model_2 == 'CIELAB': 
             color_components = self.color.to_CIELAB()
-        #elif self.color_model_2 == 'OKLAB': 
+        #elif self.color_model_1 == 'OKLAB': 
         #    color_components = self.color.to_OKLAB()
 
         self.color_model_2_component_1 = '{:.2f}'.format(color_components[0])
--- palette.py
+++ palette.py
@@ -26,14 +26,14 @@ class Palette(object):
         else:
             self._colors.pop(index)
     
-    def generate_RGB_random(self, *args):
+    def generate_sRGB_random(self, *args):
         for c in self._colors:
-            c.set_RGB((random.random(), random.random(), random.random()))
+            c.set_sRGB((random.random(), random.random(), random.random()))
     
-    def generate_RGB_cosine(self, a_r, b_r, c_r, d_r, a_g, b_g, c_g, d_g, a_b, b_b, c_b, d_b):
+    def generate_sRGB_cosine(self, a_r, b_r, c_r, d_r, a_g, b_g, c_g, d_g, a_b, b_b, c_b, d_b):
         for index in range(len(self._colors)):
             phase = index / len(self._colors)
             red = a_r + b_r * math.cos(2*math.pi * (c_r * phase + d_r))
             green = a_g + b_g * math.cos(2*math.pi * (c_g * phase + d_g))
             blue = a_b + b_b * math.cos(2*math.pi * (c_b * phase + d_b))
-            self._colors[index].set_RGB((red, green, blue))
+            self._colors[index].set_sRGB((red, green, blue))
