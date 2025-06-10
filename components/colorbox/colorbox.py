a26997475ee1e78ed37211c789e2a7e9d0536087
adamkohazi <109677549+adamkohazi@users.noreply.github.com>
2025-06-10 16:31:15 +0200

    Minor code clarity improvements

--- components/colorbox/colorbox.py
+++ components/colorbox/colorbox.py
@@ -3,17 +3,15 @@ import color
 import color_model
 
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty, OptionProperty, StringProperty, ColorProperty
+from kivy.properties import ObjectProperty, StringProperty, ColorProperty
 
-models = [model.short_name for model in color_model.color_models]
-model1 = 'sRGB'
-model2 = 'XYZ'
 
 class ColorBox(BoxLayout):
     # These need to be set from outside
     color = ObjectProperty(None)
-    color_model_1 = OptionProperty(model1, options=models)
-    color_model_2 = OptionProperty(model2, options=models)
+
+    color_model_1 = ObjectProperty(None)
+    color_model_2 = ObjectProperty(None)
 
     # Background and hex code
     background_color = ColorProperty([1, 1, 1, 1]) # Background color
@@ -39,16 +37,14 @@ class ColorBox(BoxLayout):
         self.background_color = (*self.color.get(color_model.SRGB), 1.0)
         self.rgb_hex = '#{:02X}{:02X}{:02X}'.format(*self.color.get(color_model.SRGB255))
 
-        model = next((cm for cm in color_model.color_models if cm.short_name == self.color_model_1), None)
-        color_components = self.color.get(model)
+        if self.color_model_1 is not None:
+            color_components = self.color.get(self.color_model_1)
+            self.color_model_1_component_1 = '{:.2f}'.format(color_components[0])
+            self.color_model_1_component_2 = '{:.2f}'.format(color_components[1])
+            self.color_model_1_component_3 = '{:.2f}'.format(color_components[2])
 
-        self.color_model_1_component_1 = '{:.2f}'.format(color_components[0])
-        self.color_model_1_component_2 = '{:.2f}'.format(color_components[1])
-        self.color_model_1_component_3 = '{:.2f}'.format(color_components[2])
-
-        model = next((cm for cm in color_model.color_models if cm.short_name == self.color_model_2), None)
-        color_components = self.color.get(model)
-
-        self.color_model_2_component_1 = '{:.2f}'.format(color_components[0])
-        self.color_model_2_component_2 = '{:.2f}'.format(color_components[1])
-        self.color_model_2_component_3 = '{:.2f}'.format(color_components[2])
+        if self.color_model_2 is not None:
+            color_components = self.color.get(self.color_model_2)
+            self.color_model_2_component_1 = '{:.2f}'.format(color_components[0])
+            self.color_model_2_component_2 = '{:.2f}'.format(color_components[1])
+            self.color_model_2_component_3 = '{:.2f}'.format(color_components[2])
--- components/colortable/colortable.py
+++ components/colortable/colortable.py
@@ -1,15 +1,19 @@
 
 import palette
+import color_model
 
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty, OptionProperty
-from components.colorbox.colorbox import models, model1, model2, ColorBox
+from kivy.properties import ObjectProperty, StringProperty, OptionProperty, ListProperty
+from components.colorbox.colorbox import ColorBox
 
 class ColorTable(BoxLayout):
     # These need to be set from outside
     palette = ObjectProperty(None)
-    color_model_1 = OptionProperty(model1, options=models)
-    color_model_2 = OptionProperty(model2, options=models)
+
+    spinner_values = ListProperty([model.short_name for model in color_model.color_models])
+
+    color_model_1 = StringProperty("SRGB")
+    color_model_2 = StringProperty("XYZ")
 
     def on_palette(self, instance, value):
         if isinstance(value, palette.Palette):
@@ -18,19 +22,31 @@ class ColorTable(BoxLayout):
             pass
     
     def on_color_model_1(self, instance, value):
+        model_1 = next((cm for cm in color_model.color_models if cm.short_name == self.color_model_1), None)
+
+        self.ids.cm_1_1, self.ids.cm_1_2, self.ids.cm_1_3 = model_1.component_names
+
         for row in self.ids.color_entries.children:
-            row.color_model_1 = self.color_model_1
+            row.color_model_1 = model_1
     
     def on_color_model_2(self, instance, value):
+        model_2 = next((cm for cm in color_model.color_models if cm.short_name == self.color_model_2), None)
+
+        self.ids.cm_2_1, self.ids.cm_2_2, self.ids.cm_2_3 = model_2.component_names
+
         for row in self.ids.color_entries.children:
-            row.color_model_2 = self.color_model_2
+            row.color_model_2 = model_2
 
     def update(self):
         # Set the correct number of rows
         while(len(self.ids.color_entries.children) < len(self.palette._colors)):
             # Create new row if needed
             index = len(self.ids.color_entries.children)
-            new_row = ColorBox(color = self.palette._colors[index], color_model_1 = self.color_model_1, color_model_2 = self.color_model_2)
+            new_row = ColorBox(
+                color = self.palette._colors[index],
+                color_model_1 = next((cm for cm in color_model.color_models if cm.short_name == self.color_model_1), None),
+                color_model_2 = next((cm for cm in color_model.color_models if cm.short_name == self.color_model_2), None)
+            )
             self.ids.color_entries.add_widget(new_row)
             # Bind the remove button to remove that color from the palette
             def remove_color(instance):
